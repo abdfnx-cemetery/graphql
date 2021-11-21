f0097cbaf152a2d20f58ffaa2b115b229dad9894
Abdfn <64256993+abdfnx@users.noreply.github.com>
2021-11-21 18:44:49 +0000

    add restbox graphql builds

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import typescript from "rollup-plugin-ts"
+import {lezer} from "@lezer/generator/rollup"
+
+export default {
+  input: "graphql/main.js",
+  external: id => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
+  output: [
+    {file: "dist/main.cjs", format: "cjs"},
+    {dir: "./dist", format: "es"}
+  ],
+  plugins: [lezer(), typescript()]
+}
